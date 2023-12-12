79efe4bbe647b13a5708606680e7770f8293fac2
Etienne Samson <samson.etienne@gmail.com>
2023-12-13 00:24:01 +0100

    Try REPL

--- screeps-cli.js
+++ screeps-cli.js
@@ -1,11 +1,18 @@
 #! /usr/bin/env node
 const screeps = require("@screeps/launcher");
 const readline = require("readline");
+const repl = require("repl");
 
-const rl = readline.createInterface({
+const rl = repl.start({
   input: process.stdin,
   output: process.stdout,
   prompt: "> ",
 });
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   prompt: "> ",
+// });
 
 screeps.cli("localhost", 21026, rl);
