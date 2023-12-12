f4e174a3a5f3e1c2ce017a39821107effc9d8280
Etienne Samson <samson.etienne@gmail.com>
2023-12-13 00:24:01 +0100

    Try REPL

--- server/screeps-cli.cjs
+++ server/screeps-cli.cjs
@@ -2,11 +2,18 @@
 // @ts-ignore We can't load that from the outer non-Node 10 side
 const screeps = require("@screeps/launcher");
 const readline = require("readline");
+const repl = require("repl");
 
-const rl = readline.createInterface({
+const rl = repl.start({
   input: process.stdin,
   output: process.stdout,
   prompt: "> ",
 });
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   prompt: "> ",
+// });
 
 screeps.cli("localhost", 21026, rl);
