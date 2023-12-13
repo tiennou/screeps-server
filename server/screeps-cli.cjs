20306d8cd24356bee4c77bdab141dd4856d68005
Etienne Samson <samson.etienne@gmail.com>
2023-12-13 20:06:07 +0100

    WIP

--- server/screeps-cli.cjs
+++ server/screeps-cli.cjs
@@ -1,14 +1,89 @@
 #! /usr/bin/env node
 // @ts-ignore We can't load that from the outer non-Node 10 side
 const screeps = require("@screeps/launcher");
-const readline = require("readline");
 const repl = require("repl");
+const q = require('q');
+const net = require("net");
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-const rl = repl.start({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: "> ",
-});
+cli("localhost", 21026);
+
+function cli(host, port) {
+  
+  const defer = q.defer();
+  
+  /**
+  * @param {string} cmd 
+  * @param {Context} context 
+  * @param {string} file 
+  * @param {(err: Error | null, result: any) => void} cb 
+  */
+  const rplEval = (cmd, context, file, callback) => {
+    let result;
+    try {
+      result = socket.write(cmd);
+    } catch (e) {
+      if (isRecoverableError(e)) {
+        return callback(new repl.Recoverable(e));
+      }
+    }
+    callback(null, result);
+  }
+  
+  function isRecoverableError(error) {
+    if (error.name === 'SyntaxError') {
+      return /^(Unexpected end of input|Unexpected token)/.test(error.message);
+    }
+    return false;
+  }
+  
+  const rl = repl.start({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "> ",
+    eval: rplEval,
+  });
+
+  const historyFile = path.join(os.homedir(), '.screeps-history');
+  try {
+    rl.history = JSON.parse(fs.readFileSync(historyFile));
+  } catch (err) {}
+
+  rl.on('close', () => {
+    fs.writeFileSync(historyFile, JSON.stringify(rl.history));
+  });
+  
+  rl.on('line', (line) => {
+    process.stdout.write(`socket write: ${line}\n`);
+    socket.write(line+"\r\n");
+    rl.prompt();
+  });
+
+  const socket = net.connect(port, host);
+  
+  socket.on('connect', () => {
+    defer.resolve();
+    rl.output.write(`Screeps CLI connected on ${host}:${port}.\r\n-----------------------------------------\r\n`);
+  });
+
+  socket.on('data', (data) => {
+    data = data.toString('utf8');
+    process.stdout.write(`socket read: ${data}\n`);
+    rl.output.write(data.replace(/^< /, '').replace(/\n< /, ''));
+    if(/^< /.test(data) || /\n< /.test(data)) {
+      rl.prompt();
+    }
+  });
+  
+  socket.on('error', (error) => {
+    defer.reject(error);
+  });
+  
+  return defer.promise;
+};
+
 
 // const rl = readline.createInterface({
 //   input: process.stdin,
@@ -16,4 +91,4 @@ const rl = repl.start({
 //   prompt: "> ",
 // });
 
-screeps.cli("localhost", 21026, rl);
+// screeps.cli("localhost", 21026, rl);
